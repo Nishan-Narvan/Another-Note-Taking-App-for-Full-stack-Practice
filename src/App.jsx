38010bee87a985340fb00419407475f7dae78d83
Nishan-Narvan <nishannarban001@gmail.com>
2025-08-02 23:32:47 +0530

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Example from './Email.jsx'
+import PasswordChecker from './PasswordChecker'
+
+const App = () => {
+  return (
+    <>
+    <div><Example/></div>
+  
+    <PasswordChecker/>
+    </>
+  )
+}
+
+export default App
